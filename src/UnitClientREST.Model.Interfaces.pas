00c38703f33153957c6fc49f8abcf65228dc1332
cachopaweb <cachopaweb@gmail.com>
2025-06-13 11:42:01 -0400

    add route result

--- src/UnitClientREST.Model.Interfaces.pas
+++ src/UnitClientREST.Model.Interfaces.pas
@@ -3,30 +3,33 @@ unit UnitClientREST.Model.Interfaces;
 interface
 
 uses
-  UnitObserver.Model.Interfaces, System.JSON, System.Classes;
+	UnitObserver.Model.Interfaces, System.JSON, System.Classes;
 
-  type
-    TClientResult = record
-      StatusCode: integer;
-      Content: string;
-      Error: string;
-    end;
+type
+	TClientResult = record
+		StatusCode: integer;
+		Content: string;
+		Error: string;
+    Route: string;
+	end;
 
-    iClientREST = interface
-      ['{E04FDD85-09B6-457C-9C72-7B848F33CEB0}']
-      function AddHeader(Par, Valor: string): iClientREST;
-      function AddBody(Value: string): iClientREST;overload;
-			function AddBody(Value: TJSONObject): iClientREST;overload;
-			function AddBody(Value: TStream): iClientREST;overload;
-      function AddUserPassword(User, Pass: string): iClientREST;
-      function Put(Value: string = ''): TClientResult;
-      function Get(Value: string = ''): TClientResult;
-      function Post(Value: string = ''): TClientResult;overload;
-			function Post(Value: string; Body: TJSONObject): TClientResult;overload;
-			function Post(Value: string; Body: string): TClientResult;overload;
-      function Delete(Value: string = ''): TClientResult;
-      function InscreverObservador(Value: iObservador): iClientREST;
-    end;
+	iClientREST = interface
+		['{E04FDD85-09B6-457C-9C72-7B848F33CEB0}']
+		function AddHeader(Par, Valor: string): iClientREST;
+		function AddBody(Value: string): iClientREST; overload;
+		function AddBody(Value: TJSONObject): iClientREST; overload;
+		function AddBody(Value: TStream): iClientREST; overload;
+		function AddUserPassword(User, Pass: string): iClientREST;
+		function Put(Value: string = ''): TClientResult;
+		function Get(Value: string = ''): TClientResult;
+		function Post(Value: string = ''): TClientResult; overload;
+		function Post(Value: string; Body: TJSONObject): TClientResult; overload;
+		function Post(Value: string; Body: string): TClientResult; overload;
+		function Delete(Value: string = ''): TClientResult;
+		function InscreverObservador(Value: iObservador): iClientREST;
+    function Request: TObject;
+    function Response: TObject;
+	end;
 
 implementation
 
--- src/UnitClientREST.Model.pas
+++ src/UnitClientREST.Model.pas
@@ -3,31 +3,31 @@ unit UnitClientREST.Model;
 interface
 
 uses
-  REST.Client,
-  REST.Authenticator.Basic,
-  REST.Types,
-  System.Json,
-  IpPeerClient,
-  System.Generics.Collections,
-  UnitClientREST.Model.Interfaces,
-  UnitObserver.Model.Interfaces, System.Classes;
+	REST.Client,
+	REST.Authenticator.Basic,
+	REST.Types,
+	System.Json,
+	IpPeerClient,
+	System.Generics.Collections,
+	UnitClientREST.Model.Interfaces,
+	UnitObserver.Model.Interfaces, System.Classes;
 
 type
-  TClientREST = class(TInterfacedObject, iClientREST, iSujeito)
-  private
-    FRestClient       : TRESTClient;
-    FRestRequest      : TRESTRequest;
-    FRestResponse     : TRESTResponse;
-    FListaObservers   : TList<iObservador>;
-    FListaHeaders     : TDictionary<string, string>;
-    FBasicAtentication: THTTPBasicAuthenticator;
-  public
-    constructor Create(URL: string);
-    destructor Destroy; override;
-    class function New(URL: string): iClientREST;
-    function Put(Value: string = ''): TClientResult;
-    function Get(Value: string = ''): TClientResult;
-    function Post(Value: string = ''): TClientResult; overload;
+	TClientREST = class(TInterfacedObject, iClientREST, iSujeito)
+	private
+		FRestClient       : TRESTClient;
+		FRestRequest      : TRESTRequest;
+		FRestResponse     : TRESTResponse;
+		FListaObservers   : TList<iObservador>;
+		FListaHeaders     : TDictionary<string, string>;
+		FBasicAtentication: THTTPBasicAuthenticator;
+	public
+		constructor Create(URL: string);
+		destructor Destroy; override;
+		class function New(URL: string): iClientREST;
+		function Put(Value: string = ''): TClientResult;
+		function Get(Value: string = ''): TClientResult;
+		function Post(Value: string = ''): TClientResult; overload;
 		function Post(Value: string; Body: TJSONObject): TClientResult; overload;
 		function Post(Value: string; Body: string): TClientResult; overload;
 		function Delete(Value: string = ''): TClientResult;
@@ -39,34 +39,36 @@ type
 		function AddBody(Value: string): iClientREST; overload;
 		function AddBody(Value: TJSONObject): iClientREST; overload;
 		function AddBody(Value: TStream): iClientREST; overload;
-    function AddUserPassword(User: string; Pass: string): iClientREST;
-  end;
+		function AddUserPassword(User: string; Pass: string): iClientREST;
+    function Request: TObject;
+    function Response: TObject;
+	end;
 
 implementation
 
 uses
-  System.SysUtils;
+	System.SysUtils;
 
 { TClienteREST }
 
 function TClientREST.AddObservador(Value: iObservador): iSujeito;
 begin
-  Result := Self;
-  FListaObservers.Add(Value);
+	Result := Self;
+	FListaObservers.Add(Value);
 end;
 
 function TClientREST.AddUserPassword(User, Pass: string): iClientREST;
 begin
-  Result                    := Self;
-  FBasicAtentication        := THTTPBasicAuthenticator.Create(User, Pass);
-  FRestClient.Authenticator := FBasicAtentication;
+	Result                    := Self;
+	FBasicAtentication        := THTTPBasicAuthenticator.Create(User, Pass);
+	FRestClient.Authenticator := FBasicAtentication;
 end;
 
 function TClientREST.AddBody(Value: string): iClientREST;
 begin
 	Result := Self;
-  FRestRequest.ClearBody;
-  FRestRequest.AddBody(Value);
+	FRestRequest.ClearBody;
+	FRestRequest.AddBody(Value);
 end;
 
 function TClientREST.AddBody(Value: TJSONObject): iClientREST;
@@ -85,229 +87,251 @@ end;
 
 function TClientREST.AddHeader(Par, Valor: string): iClientREST;
 begin
-  Result := Self;
-  if not Assigned(FListaHeaders) then
-    FListaHeaders := TDictionary<string, string>.Create;
-  FListaHeaders.Add(Par, Valor);
+	Result := Self;
+	if not Assigned(FListaHeaders) then
+		FListaHeaders := TDictionary<string, string>.Create;
+	FListaHeaders.Add(Par, Valor);
 end;
 
 constructor TClientREST.Create(URL: string);
 begin
-  FListaObservers       := TList<iObservador>.Create;
-  FListaHeaders         := TDictionary<string, string>.Create;
-  FRestClient           := TRESTClient.Create(URL);
-  FRestRequest          := TRESTRequest.Create(nil);
-  FRestResponse         := TRESTResponse.Create(nil);
-  FRestRequest.Client   := FRestClient;
-  FRestRequest.Response := FRestResponse;
+	FListaObservers       := TList<iObservador>.Create;
+	FListaHeaders         := TDictionary<string, string>.Create;
+	FRestClient           := TRESTClient.Create(URL);
+	FRestRequest          := TRESTRequest.Create(nil);
+	FRestResponse         := TRESTResponse.Create(nil);
+	FRestRequest.Client   := FRestClient;
+	FRestRequest.Response := FRestResponse;
 end;
 
 function TClientREST.Delete(Value: string = ''): TClientResult;
 var
-  Jo: TJSONObject;
+	Jo: TJSONObject;
 begin
-  try
-    FRestRequest.Method := rmDELETE;
-    FRestRequest.Execute;
-    Result.Content    := FRestResponse.Content;
-    Result.StatusCode := FRestResponse.StatusCode;
-  except
-    on E: Exception do
-    begin
-      Result.Content := FRestResponse.Content;
-      Result.Error   := E.Message;
-      Result.StatusCode := FRestResponse.StatusCode;
-    end;
-  end;
+	try
+		FRestRequest.Method := rmDELETE;
+		FRestRequest.Execute;
+		Result.Content    := FRestResponse.Content;
+		Result.StatusCode := FRestResponse.StatusCode;
+    Result.Route      := FRestClient.BaseURL;
+	except
+		on E: Exception do
+		begin
+			Result.Content    := FRestResponse.Content;
+			Result.Error      := E.Message;
+			Result.StatusCode := FRestResponse.StatusCode;
+      Result.Route      := FRestClient.BaseURL;
+		end;
+	end;
 end;
 
 destructor TClientREST.Destroy;
 begin
-  FreeAndNil(FRestClient);
-  FreeAndNil(FRestRequest);
-  FreeAndNil(FRestResponse);
-  FreeAndNil(FListaObservers);
-  if Assigned(FListaHeaders) then
-    FreeAndNil(FListaHeaders);
-  if Assigned(FBasicAtentication) then
-    FBasicAtentication.DisposeOf;
-  inherited;
+	FreeAndNil(FRestClient);
+	FreeAndNil(FRestRequest);
+	FreeAndNil(FRestResponse);
+	FreeAndNil(FListaObservers);
+	if Assigned(FListaHeaders) then
+		FreeAndNil(FListaHeaders);
+	if Assigned(FBasicAtentication) then
+		FBasicAtentication.DisposeOf;
+	inherited;
 end;
 
 function TClientREST.Get(Value: string = ''): TClientResult;
 var
-  chave: string;
-  Valor: string;
+	chave: string;
+	Valor: string;
 begin
-  try
-    if Value <> '' then
-      FRestClient.BaseURL := Value;
-    FRestRequest.Method   := rmGET;
-    for chave in FListaHeaders.Keys do
-    begin
-      FListaHeaders.TryGetValue(chave, Valor);
-      FRestRequest.Params.AddHeader(chave, Valor);
-      FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
-    end;
-    FRestRequest.Execute;
-    Result.Content    := FRestResponse.Content;
-    Result.StatusCode := FRestResponse.StatusCode;
-  except
-    on E: Exception do
-    begin
-      Result.Content    := FRestResponse.Content;
-      Result.Error      := E.Message;
-      Result.StatusCode := FRestResponse.StatusCode;
-    end;
-  end;
+	try
+		if Value <> '' then
+			FRestClient.BaseURL := Value;
+		FRestRequest.Method   := rmGET;
+		for chave in FListaHeaders.Keys do
+		begin
+			FListaHeaders.TryGetValue(chave, Valor);
+			FRestRequest.Params.AddHeader(chave, Valor);
+			FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
+		end;
+		FRestRequest.Execute;
+		Result.Content    := FRestResponse.Content;
+		Result.StatusCode := FRestResponse.StatusCode;
+    Result.Route      := FRestClient.BaseURL;
+	except
+		on E: Exception do
+		begin
+			Result.Content    := FRestResponse.Content;
+			Result.Error      := E.Message;
+			Result.StatusCode := FRestResponse.StatusCode;
+      Result.Route      := FRestClient.BaseURL;
+		end;
+	end;
 end;
 
 function TClientREST.InscreverObservador(Value: iObservador): iClientREST;
 begin
-  Result := Self;
-  AddObservador(Value);
+	Result := Self;
+	AddObservador(Value);
 end;
 
 class function TClientREST.New(URL: string): iClientREST;
 begin
-  Result := Self.Create(URL);
+	Result := Self.Create(URL);
 end;
 
 function TClientREST.Notificar(Value: TNotificacao): iSujeito;
 var
-  i: Integer;
+	i: Integer;
 begin
-  for i := 0 to Pred(FListaObservers.Count) do
-    FListaObservers[i].Atualizar(Value);
+	for i := 0 to Pred(FListaObservers.Count) do
+		FListaObservers[i].Atualizar(Value);
 end;
 
 function TClientREST.Post(Value: string = ''): TClientResult;
 var
-  Jo   : TJSONObject;
-  chave: string;
-  Valor: string;
+	Jo   : TJSONObject;
+	chave: string;
+	Valor: string;
 begin
-  try
-    if Value <> '' then
-      FRestClient.BaseURL := Value;
-    FRestRequest.Method   := rmPOST;
-    for chave in FListaHeaders.Keys do
-    begin
-      FListaHeaders.TryGetValue(chave, Valor);
-      FRestRequest.Params.AddHeader(chave, Valor);
-      FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
-    end;
-    FRestRequest.Execute;
-    FListaHeaders.Clear;
-    Result.Content    := FRestResponse.Content;
-    Result.StatusCode := FRestResponse.StatusCode;
-  except
-    on E: Exception do
-    begin
-      Result.Content := FRestResponse.Content;
-      Result.Error   := E.Message;
-      Result.StatusCode := FRestResponse.StatusCode;
-    end;
-  end;
+	try
+		if Value <> '' then
+			FRestClient.BaseURL := Value;
+		FRestRequest.Method   := rmPOST;
+		for chave in FListaHeaders.Keys do
+		begin
+			FListaHeaders.TryGetValue(chave, Valor);
+			FRestRequest.Params.AddHeader(chave, Valor);
+			FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
+		end;
+		FRestRequest.Execute;
+		FListaHeaders.Clear;
+		Result.Content    := FRestResponse.Content;
+		Result.StatusCode := FRestResponse.StatusCode;
+    Result.Route      := FRestClient.BaseURL;
+	except
+		on E: Exception do
+		begin
+			Result.Content    := FRestResponse.Content;
+			Result.Error      := E.Message;
+			Result.StatusCode := FRestResponse.StatusCode;
+      Result.Route      := FRestClient.BaseURL;
+		end;
+	end;
 end;
 
 function TClientREST.Post(Value: string; Body: TJSONObject): TClientResult;
 var
-  Jo   : TJSONObject;
-  chave: string;
-  Valor: string;
+	Jo   : TJSONObject;
+	chave: string;
+	Valor: string;
 begin
-  try
-    if Assigned(Body) then
-      AddBody(Body);
-    if Value <> '' then
-      FRestClient.BaseURL := Value;
-    FRestRequest.Method   := rmPOST;
-    for chave in FListaHeaders.Keys do
-    begin
-      FListaHeaders.TryGetValue(chave, Valor);
-      FRestRequest.Params.AddHeader(chave, Valor);
-      FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
-    end;
-    FRestRequest.Execute;
-    FListaHeaders.Clear;
-    Result.Content    := FRestResponse.Content;
-    Result.StatusCode := FRestResponse.StatusCode;
-  except
-    on E: Exception do
-    begin
-      Result.Content := FRestResponse.Content;
-      Result.Error   := E.Message;
-      Result.StatusCode := FRestResponse.StatusCode;
-    end;
+	try
+		if Assigned(Body) then
+			AddBody(Body);
+		if Value <> '' then
+			FRestClient.BaseURL := Value;
+		FRestRequest.Method   := rmPOST;
+		for chave in FListaHeaders.Keys do
+		begin
+			FListaHeaders.TryGetValue(chave, Valor);
+			FRestRequest.Params.AddHeader(chave, Valor);
+			FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
+		end;
+		FRestRequest.Execute;
+		FListaHeaders.Clear;
+		Result.Content    := FRestResponse.Content;
+		Result.StatusCode := FRestResponse.StatusCode;
+    Result.Route      := FRestClient.BaseURL;
+	except
+		on E: Exception do
+		begin
+			Result.Content    := FRestResponse.Content;
+			Result.Error      := E.Message;
+			Result.StatusCode := FRestResponse.StatusCode;
+      Result.Route      := FRestClient.BaseURL;
+		end;
 	end;
 end;
 
 function TClientREST.RemoveObservador(Value: iObservador): iSujeito;
 begin
-  Result := Self;
-  FListaObservers.Remove(Value);
+	Result := Self;
+	FListaObservers.Remove(Value);
+end;
+
+function TClientREST.Request: TObject;
+begin
+	Result := FRestRequest;
+end;
+
+function TClientREST.Response: TObject;
+begin
+  Result := FRestResponse;
 end;
 
 function TClientREST.Post(Value, Body: string): TClientResult;
 var
-  Jo   : TJSONObject;
-  chave: string;
-  Valor: string;
+	Jo   : TJSONObject;
+	chave: string;
+	Valor: string;
 begin
-  try
-    if Body <> '' then
-      AddBody(Body);
-    if Value <> '' then
-      FRestClient.BaseURL := Value;
-    FRestRequest.Method   := rmPOST;
-    for chave in FListaHeaders.Keys do
-    begin
-      FListaHeaders.TryGetValue(chave, Valor);
-      FRestRequest.Params.AddHeader(chave, Valor);
-      FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
-    end;
-    FRestRequest.Execute;
-    FListaHeaders.Clear;
-    Result.Content    := FRestResponse.Content;
-    Result.StatusCode := FRestResponse.StatusCode;
-  except
-    on E: Exception do
-    begin
-      Result.Content := FRestResponse.Content;
-      Result.Error   := E.Message;
-      Result.StatusCode := FRestResponse.StatusCode;
-    end;
-  end;
+	try
+		if Body <> '' then
+			AddBody(Body);
+		if Value <> '' then
+			FRestClient.BaseURL := Value;
+		FRestRequest.Method   := rmPOST;
+		for chave in FListaHeaders.Keys do
+		begin
+			FListaHeaders.TryGetValue(chave, Valor);
+			FRestRequest.Params.AddHeader(chave, Valor);
+			FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
+		end;
+		FRestRequest.Execute;
+		FListaHeaders.Clear;
+		Result.Content    := FRestResponse.Content;
+		Result.StatusCode := FRestResponse.StatusCode;
+    Result.Route      := FRestClient.BaseURL;
+	except
+		on E: Exception do
+		begin
+			Result.Content    := FRestResponse.Content;
+			Result.Error      := E.Message;
+			Result.StatusCode := FRestResponse.StatusCode;
+      Result.Route      := FRestClient.BaseURL;
+		end;
+	end;
 end;
 
 function TClientREST.Put(Value: string = ''): TClientResult;
 var
-  chave: string;
-  Valor: string;
+	chave: string;
+	Valor: string;
 begin
-  try
-    if Value <> '' then
-      FRestClient.BaseURL := Value;
-    FRestRequest.Method   := rmPUT;
-    for chave in FListaHeaders.Keys do
-    begin
-      FListaHeaders.TryGetValue(chave, Valor);
-      FRestRequest.Params.AddHeader(chave, Valor);
-      FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
-    end;
-    FRestRequest.Execute;
-    FListaHeaders.Clear;
-    Result.Content    := FRestResponse.Content;
-    Result.StatusCode := FRestResponse.StatusCode;
-  except
-    on E: Exception do
-    begin
-      Result.Content    := FRestResponse.Content;
-      Result.Error      := E.Message;
-      Result.StatusCode := FRestResponse.StatusCode;
-    end;
-  end;
+	try
+		if Value <> '' then
+			FRestClient.BaseURL := Value;
+		FRestRequest.Method   := rmPUT;
+		for chave in FListaHeaders.Keys do
+		begin
+			FListaHeaders.TryGetValue(chave, Valor);
+			FRestRequest.Params.AddHeader(chave, Valor);
+			FRestRequest.Params.ParameterByName(chave).Options := [poDoNotEncode];
+		end;
+		FRestRequest.Execute;
+		FListaHeaders.Clear;
+		Result.Content    := FRestResponse.Content;
+		Result.StatusCode := FRestResponse.StatusCode;
+    Result.Route      := FRestClient.BaseURL;
+	except
+		on E: Exception do
+		begin
+			Result.Content    := FRestResponse.Content;
+			Result.Error      := E.Message;
+			Result.StatusCode := FRestResponse.StatusCode;
+      Result.Route      := FRestClient.BaseURL;
+		end;
+	end;
 end;
 
 end.
